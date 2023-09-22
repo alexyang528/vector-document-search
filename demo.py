e36e083e76fa781a3c1b361d10709ca9dd9d3a69
alexyang528 <alexsyang@icloud.com>
2023-09-22 16:04:26 -0400

    Refactor card rendering

--- demo.py
+++ demo.py
@@ -39,57 +39,33 @@ def clean_search_results(dsg_client, segment, prompt="clean_segment_prompt.md"):
     return cleaned_results
 
 
-def result_card(result, body_field=[], dsg_client=None):
+def vector_result_card(result):
     name = result["data"]["name"]
+    segment = "... " + result["segment"]["text"].strip() + " ..."
+    score = result["segment"]["score"]
 
-    # Get the body value (e.g., 'bodyV2.markdown')
-    body_value = result["data"]
-    for field in body_field:
-        if field in body_value:
-            body_value = body_value[field]
+    template = f"""
+    <div style="border-radius: 5px; background-color: #f2f2f2; padding: 10px; box-shadow: 2px 2px 10px rgba(0,0,0,0.1);">
+        <div style="font-size: 18px; font-weight: bold;">{name}</div>
+        <p style="margin: 10px 0;">{segment}</p>
+        <p style="text-align: right;"><i>Score: {score}</i></p>
+    </div>
+    """
 
-    # For vector search results
-    if "segment" in result:
-        segment = "... " + result["segment"]["text"].strip() + " ..."
-        score = result["segment"]["score"]
+    return st.write(template, unsafe_allow_html=True)
 
-        # If the name segment, replace it with the body field
-        if name == segment:
-            segment = body_value[:250] + " ..."
-            score = str(result["segment"]["score"]) + " (matched on name)"
-        
-        # If recieved a DSG client, clean the segment
-        if dsg_client:
-            segment = clean_search_results(dsg_client, segment)
 
-        template = f"""
-        <div style="border-radius: 5px; background-color: #f2f2f2; padding: 10px; box-shadow: 2px 2px 10px rgba(0,0,0,0.1);">
-            <div style="font-size: 18px; font-weight: bold;">{name}</div>
-            <p style="margin: 10px 0;">{segment}</p>
-            <p style="text-align: right;"><i>Score: {score}</i></p>
-        </div>
-        """
+def regular_result_card(result):
+    name = result["data"]["name"]
+    segment = result["data"].get("s_snippet", "")
 
-        return st.write(template, unsafe_allow_html=True)
-    
-    # For current results, just use the body value
-    else:
-        if result["data"].get("s_snippet"):
-            segment = result["data"].get("s_snippet")
-        else:
-            segment = body_value[:250] + " ..."
-
-        # If recieved a DSG client, clean the segment
-        if dsg_client:
-            segment = clean_search_results(dsg_client, segment)
-
-        template = f"""
-        <div style="border-radius: 5px; background-color: #f2f2f2; padding: 10px; box-shadow: 2px 2px 10px rgba(0,0,0,0.1);">
-            <div style="font-size: 18px; font-weight: bold;">{name}</div>
-            <p style="margin: 10px 0;">{segment}</p>
-        </div>
-        """
-        return st.write(template, unsafe_allow_html=True)
+    template = f"""
+    <div style="border-radius: 5px; background-color: #f2f2f2; padding: 10px; box-shadow: 2px 2px 10px rgba(0,0,0,0.1);">
+        <div style="font-size: 18px; font-weight: bold;">{name}</div>
+        <p style="margin: 10px 0;">{segment}</p>
+    </div>
+    """
+    return st.write(template, unsafe_allow_html=True)
 
 
 def direct_answer_card(direct_answer, is_chat=False):
@@ -128,155 +104,143 @@ def direct_answer_card(direct_answer, is_chat=False):
     return st.write(template, unsafe_allow_html=True)
 
 
-DEMOS = {
-    "harry_potter": {
+def render_results(client, query, demo, vector=True):
+
+    if vector:
+        vertical_key = demo["vertical_key"]
+        result_card = vector_result_card
+    else:
+        vertical_key = demo["current_vertical_key"]
+        result_card = regular_result_card
+
+    # Get search results
+    start = time.time()
+    response = search_request(client, query, demo["experience_key"], vertical_key)
+    end = time.time()
+    response_time = end - start
+
+    # Parse results
+    results = response["response"].get("results", [])
+    results_count = response["response"].get("resultsCount", 0)
+    direct_answer = response["response"].get("directAnswer", None)
+
+    # Render results
+    st.write(f"_{results_count} results ({response_time:.2f} seconds)_")
+    if direct_answer:
+        direct_answer_card(direct_answer)
+        st.write("---")
+
+        # Logic to boost the direct answer result to the top
+        # related_result = direct_answer["relatedItem"]["data"]["uid"]
+
+        # for result in results:
+        #     if result["data"]["uid"] == related_result and direct_answer["answer"]["snippet"]["value"] in result["segment"]["text"]:
+        #         result_card(result, body_field, dsg_client)
+        #         st.write("---")
+        #         results.remove(result)
+        #         break
+
+    for result in results:
+        result_card(result)
+        st.write("---")
+    
+    return response
+
+
+DEMOS = [
+    {
         "name": "Harry Potter Books",
         "api_key": st.secrets["book-search"]["api_key"],
         "chat_api_key": st.secrets["book-search"]["chat_api_key"],
         "experience_key": "book-search",
         "vertical_key": "books",
         "current_vertical_key": "books_current",
-        "body_field": ["c_file", "s_content"],
-        "default_search": "",
+        "body_fields": [["c_file", "s_content"]],
+        "default_search": "Who is Albus Dumbledore?",
     },
-    "samsung": {
+    {
         "name": "Samsung Troubleshooting Guides",
         "api_key": st.secrets["samsung-troubleshooting-search"]["api_key"],
         "chat_api_key": st.secrets["samsung-troubleshooting-search"]["chat_api_key"],
         "experience_key": "samsung-troubleshooting-search",
         "vertical_key": "guides",
         "current_vertical_key": "guides_current",
-        "body_field": ["bodyV2", "markdown"],
+        "body_fields": [["bodyV2", "markdown"]],
         "default_search": ""
     },
-    "iceberg_reports": {
+    {
         "name": "Iceberg Reports",
         "api_key": st.secrets["iceberg-reports"]["api_key"],
+        "chat_api_key": None,
         "experience_key": "iceberg-iq-report-search",
         "vertical_key": "reports",
-        "body_field": [],
+        "current_vertical_key": None,
+        "body_fields": [],
+        "default_search": "",
+    },
+    {
+        "name": "Cox Manuals",
+        "api_key": st.secrets["cox-manuals"]["api_key"],
+        "chat_api_key": None,
+        "experience_key": "cox-residential-answers-for-chat",
+        "vertical_key": "manuals_doc_search",
+        "current_vertical_key": None,
+        "body_fields": [],
+        "default_search": "",
+    },
+    {
+        "name": "Hitchhikers",
+        "api_key": st.secrets["hitchhikers"]["api_key"],
+        "chat_api_key": None,
+        "experience_key": "yext-help-hitchhikers-vector-search",
+        "vertical_key": "content",
+        "current_vertical_key": "content_current",
+        "body_fields": [["body"], ["description"]],
         "default_search": "",
     }
-}
+]
 
-# Populate sidebar options
-st.sidebar.write("## Demo Options")
-demo = st.sidebar.selectbox("Select Demo", list(DEMOS.keys()))
-show_current = st.sidebar.checkbox("Compare Non-Vector Results", value=False, disabled="current_vertical_key" not in DEMOS[demo])
-# Removing option, because very slow
+# Select demo
+st.sidebar.write("## Select Demo")
+demo = st.sidebar.selectbox(label="Select Demo", options=DEMOS, format_func=lambda x: x["name"])
+
+# Populate other values
+show_current = st.sidebar.checkbox("Compare Non-Vector Results", value=False, disabled=not demo["current_vertical_key"])
+# chat_direct_answer = st.sidebar.checkbox("Generate Chat Direct Answer", value=False, disabled=not demo["chat_api_key"])
 # gpt_cleaning = st.sidebar.checkbox("Use GPT Cleaning", value=False)
-chat_direct_answer = st.sidebar.checkbox("Generate Chat Direct Answer", value=False, disabled="chat_api_key" not in DEMOS[demo])
 
-# Calculate values
-client = init_yext_client(DEMOS[demo]["api_key"])
-dsg_client = None
-if chat_direct_answer:
-    chat_client = init_yext_client(DEMOS[demo]["chat_api_key"])
-# if gpt_cleaning: # Only if the user wants to clean segments
-#     dsg_client = init_dsg_client(DEMOS[demo]["api_key"])
-experience_key = DEMOS[demo]["experience_key"]
-vertical_key = DEMOS[demo]["vertical_key"]
-body_field = DEMOS[demo]["body_field"]
-query = st.text_input(label=f"Search {DEMOS[demo]['name']}:", value=DEMOS[demo]["default_search"])
+# Initialize clients
+client = init_yext_client(demo["api_key"])
+# dsg_client = init_dsg_client(demo["api_key"])
+# chat_client = init_yext_client(demo["chat_api_key"])
 
-# Fetch results for vector search
+# Query input
+query = st.text_input(label=f"Search {demo['name']}:", value=demo["default_search"])
+
+# Fetch and render results
 if query:
-    start = time.time()
-    response = search_request(client, query, experience_key, vertical_key)
-    end = time.time()
-    response_time = end - start
-    results = response["response"].get("results", [])
-    results_count = response["response"].get("resultsCount", 0)
-    if chat_direct_answer:
-        direct_answer = chat_client.chat_message(query, results, experience_key)
-    else:
-        direct_answer = response["response"].get("directAnswer", None)
-
-    # Get results for non-vector search
-    if show_current:
-        start = time.time()
-        current_response = search_request(client, query, experience_key, DEMOS[demo]["current_vertical_key"])
-        end = time.time()
-        current_response_time = end - start
-        current_results = current_response["response"].get("results", [])
-        current_results_count = current_response["response"].get("resultsCount", 0)
-        if chat_direct_answer:
-            current_direct_answer = chat_client.chat_message(query, current_results, experience_key)
-        else:
-            current_direct_answer = current_response["response"].get("directAnswer", None)
-
-    # Write API response
-    st.sidebar.write("## API Responses")
-    with st.sidebar.expander("View Raw Response"):
-        st.write(response)
-    if show_current:
-        with st.sidebar.expander("View Non-Vector Raw Response"):
-            st.write(current_response)
-
-
-    # Render results
     if show_current:
         vector, current = st.columns(2, gap="medium")
 
         with vector:
             st.write("### Vector Document Search")
-            if query:
-                st.write(f"_{results_count} results ({response_time:.2f} seconds)_")
-            if direct_answer:
-                direct_answer_card(direct_answer, chat_direct_answer)
-                st.write("---")
-
-                # related_result = direct_answer["relatedItem"]["data"]["uid"]
-
-                # for result in results:
-                #     if result["data"]["uid"] == related_result and direct_answer["answer"]["snippet"]["value"] in result["segment"]["text"]:
-                #         result_card(result, body_field, dsg_client)
-                #         st.write("---")
-                #         results.remove(result)
-                #         break
-
-            for result in results:
-                result_card(result, body_field, dsg_client)
-                st.write("---")
+            vector_response = render_results(client, query, demo)
 
         with current:
             st.write("### Non-Vector Search")
-            if query:
-                st.write(f"_{current_results_count} results ({current_response_time:.2f} seconds)_")
-            if current_direct_answer:
-                direct_answer_card(current_direct_answer, chat_direct_answer)
-                st.write("---")
+            current_response = render_results(client, query, demo, vector=False)
 
-                # related_result = current_direct_answer["relatedItem"]["data"]["uid"]
-
-                # for result in current_results:
-                #     if result["data"]["uid"] == related_result:
-                #         result_card(result, body_field, dsg_client)
-                #         st.write("---")
-                #         current_results.remove(result)
-                #         break
-
-            for result in current_results:
-                result_card(result, body_field, dsg_client)
-                st.write("---")        
+        # Write API response
+        st.sidebar.write("## API Responses")
+        with st.sidebar.expander("View Vector Response"):
+            st.write(vector_response)
+        with st.sidebar.expander("View Non-Vector Response"):
+            st.write(current_response)
 
     else:
-        if query:
-            st.write(f"_{results_count} results ({response_time:.2f} seconds)_")
-        if direct_answer:
-            direct_answer_card(direct_answer, chat_direct_answer)
-            st.write("---")
+        response = render_results(client, query, demo)
 
-            # related_result = direct_answer["relatedItem"]["data"]["uid"]
-
-            # for result in results:
-            #     if result["data"]["uid"] == related_result and direct_answer["answer"]["snippet"]["value"] in result["segment"]["text"]:
-            #         result_card(result, body_field, dsg_client)
-            #         st.write("---")
-            #         results.remove(result)
-            #         break
-
-        for result in results:
-            result_card(result, body_field, dsg_client)
-            st.write("---")
-            
+        # Write API response
+        st.sidebar.write("## API Responses")
+        with st.sidebar.expander("View Response"):
+            st.write(response)
